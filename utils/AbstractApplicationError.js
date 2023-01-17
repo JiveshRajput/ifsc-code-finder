913f361c8fba7982ca7b884c7d1ce31191869199
Jivesh <jsoperatorz@gmail.com>
2023-01-17 12:40:44 +0530

    updated css of search box container of ifsc finder

--- utils/AbstractApplicationError.js
+++ utils/AbstractApplicationError.js
@@ -1,18 +1,3 @@
-// class AbstractApplicationError extends Error {
-// 	constructor(message, statusCode) {
-// 		super(message);
-// 		this.statusCode = statusCode;
-// 		this.status = `${statusCode}`.startsWith("4")
-// 			? "Failure"
-// 			: "Internal Server Error";
-// 		this.beingHandled = true;
-// 		console.log(this.statusCode, message);
-
-// 	}
-// }
-
-// export default AbstractApplicationError;
-
 export default async function AbstractApplicationError(
 	message,
 	statusCode,
